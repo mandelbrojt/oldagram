9c8d52b7717a8cb5f9e90897c2b2072a16a7c600
luis <luis.moreno@exatec.tec.mx>
2024-04-01 22:17:45 -0600

    Update: Posts rendered dynamically

--- index.js
+++ index.js
@@ -27,3 +27,41 @@ const posts = [
         likes: 152
     }
 ]
+
+function createContent() {
+    let pageContent = ""
+    for (let obj of posts) {
+        pageContent += `
+        <section class="post-container">
+            <div class="post-header">
+                <img id="post-avatar" class="user-avatar" src="${obj.avatar}" alt="User profile picture">
+                <div class="user-details">
+                    <p id="post-header-title">${obj.name}</p>
+                    <p id="post-header-location">${obj.location}</p>
+                </div>
+            </div>
+            <div class="post-img-container">
+                <img id="post-img" src="${obj.post}">
+            </div>
+            <div class="post-interactions">
+                <img id="like-btn" class="interaction-btn" src="images/icon-heart.png">
+                <img id="comment-btn" class="interaction-btn" src="images/icon-comment.png">
+                <img id="share-btn" class="interaction-btn" src="images/icon-dm.png">
+            </div>
+            <div class="post-activity">
+                <p id="post-likes"><span id="num-likes">${obj.likes}</span> likes</p>
+                <p id="usr-comment"><span id="usr-name">${obj.username}</span> ${obj.comment}</p>
+            </div>
+        </section>
+        `
+    }
+    return pageContent
+}
+
+function renderContent() {
+    let mainContent = createContent()
+    let mainElement = document.querySelector("main")
+    mainElement.innerHTML = mainContent
+}
+
+renderContent()
